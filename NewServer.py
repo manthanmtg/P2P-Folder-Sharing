a83d91a799f1dc265057c28d5e55dd9ed89e1327
Manthan B Y <manthan.gowda@gmail.com>
2019-10-25 12:35:58 +0530

    Added logging, bootstrap, some code changes with beautifying the code, Added more documentation with comments

--- NewServer.py
+++ NewServer.py
@@ -6,6 +6,7 @@ Run the program using: python3 NewServer.py
 
 @author: Manthan B Y, Abhishek Patil, Adeesh
 """
+# import all the required modules and code snippets here...
 import time
 import os
 from os import path
@@ -15,6 +16,13 @@ from socket import *
 from html_snips import *
 from resp_header import get_header
 from my_zipper import create_zip
+import logging
+
+logging.basicConfig(filename="server.log", 
+                    format='%(asctime)s %(message)s', 
+                    filemode='w') 
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG) 
 
 home_dir = os.environ['HOME']
 times = 0
@@ -24,11 +32,11 @@ serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(serverAddr)
 
 response = bytes()      # reponse bytes object
-serverSocket.listen(1)
+serverSocket.listen(5)
 prev_req_name = ''
 prev_req_time = -1000   # previous request time to handle duplicate requests
 while 1:
-    print('The server lsitening at', serverPort)
+    logger.info('The server lsitening at ' + str(serverPort))
     connectionSocket, addr = serverSocket.accept()
     data = connectionSocket.recv(1024).decode() # receiving the request data from the client
     if(data == ''):         # if the data received is empty don't proceed
@@ -41,24 +49,26 @@ while 1:
         continue
     prev_req_name = req_name
     prev_req_time = curr_req_time
-    print("\n------------ Connection Accepted ----------------")    
-    print('=== Request from ' + str(addr) + ' ===')
-    print(data)
+    logger.info("Connection Accepted: Client: " + str(addr))
+    # use the below print statement to print the request ( for debugging )
+    # print(data)
     
     resp_data = html_start
-    print("*******" + req_name + "*********")
+    logger.info("Requested  File: " + str(req_name))
+    # ? Check whether the request is to download
     if(req_name[-3:] == "get"):
         file_path = home_dir + req_name[:-3]
-        print("file Path: ", file_path)
+        # print file path ( for debugging )
+        # print("file Path: ", file_path)
         mime = MimeTypes()
         mime_type = mime.guess_type(file_path)[0]
         if(mime_type == None):
             mime_type = 'text/plain'
-        response = get_header(200, 'download', mime_type, os.path.getsize(file_path), req_name[1:-3]).encode()
+        # ? Check whether the request is directory
         if(os.path.isdir(file_path)):
             create_zip(req_name[1:-3], file_path)
             data = ''
-            with open(req_name[1:-3] + '.zip', "rb") as f:
+            with open(req_name[1:-3] + '.zip', 'rb') as f:
                 data = f.read()     # reading the file in the binary format
             zip_path = os.getcwd() + '/' + req_name[1:-3] + ".zip"
             mime_type = mime.guess_type(zip_path)[0]
@@ -67,35 +77,45 @@ while 1:
             response = get_header(200, 'download', mime_type, os.path.getsize(zip_path), req_name[1:-3] + ".zip").encode()
             response += data    # attaching the binary format of the text to the http response
             os.remove(zip_path)
-            print(response)
+        # ? Else it's not a direcrory, it's a file :)
         else:
             with open(file_path, "rb") as f:
                 data = f.read()     # reading the file in the binary format
+                response = get_header(200, 'download', mime_type, os.path.getsize(file_path), req_name[1:-3]).encode()
                 response += data    # attaching the binary format of the text to the http response
-            print(response)
+        # use the below print statement to print the statement ( for debugging )
+        # print(response)
         connectionSocket.send(response)
+        logger.info("Request successfully served: Resposne Sent with the requested file atatched")
         connectionSocket.close()
-        continue
+        logger.info("Connection Closed: Connection with client " + str(addr) + " closed")
+        continue    # after no point in moving forward :)
     curr_req_name = home_dir + req_name
+    # ? Check whether the request is to list the contents of the directory
     if(os.path.isdir(curr_req_name)):
-        resp_data += "<h1> Here are the list of files</h1> <br>"
-        resp_data += "<ul>"
+        resp_data += """<div class="container"><div class="page-header"><h1> Here are the list of files </h1></div>"""
+        resp_data += """<table class="table">"""
+        resp_data += """<thead class="thead-dark"><tr><th scope="col">File/Dir</th><th scope="col">Action</th></tr></thead><tbody>"""
         for content in os.listdir(curr_req_name):
             curr_full_path = curr_req_name.rstrip('/') + '/' + content
-            print(curr_full_path)
-            if(os.path.isfile(curr_full_path)):
-                resp_data += "<li>" + """<a href = "{}">""".format(curr_full_path.replace("/home/manthanby", "", 1)) +  content + """</a>""" + """ Download """ +  """<a href="{}get" target="_blank">""".format(curr_full_path.replace("/home/manthanby", "", 1)) + """GET</a>""" + "</li>"
-            else:
-                resp_data += "<li>" + """<a href = "{}">""".format(curr_full_path.replace("/home/manthanby", "", 1)) +  content + """</a>""" + """ Download """ +  """<a href="{}get" target="_blank">""".format(curr_full_path.replace("/home/manthanby", "", 1)) + """GET</a>""" + "</li>"
-        resp_data += "</ul>"        
-    
-    
+            # for debugging
+            # print(curr_full_path)
+            # ? Check whether it is a file
+            #if(os.path.isfile(curr_full_path)):
+            resp_data += "<tr>" + """<td><a href = "{}">""".format(curr_full_path.replace(home_dir, "", 1)) +  content + """</a></td>""" + """<td><a href="{}get" target="_blank">""".format(curr_full_path.replace(home_dir, "", 1)) + """GET</a></td>""" + "</tr>"
+            #else:
+            # resp_data += "<li>" + """<a href = "{}">""".format(curr_full_path.replace("/home/manthanby", "", 1)) +  content + """</a>""" + """ Download """ +  """<a href="{}get" target="_blank">""".format(curr_full_path.replace("/home/manthanby", "", 1)) + """GET</a>""" + "</li>"
+        resp_data += "</tbody></table></div>"        
+
     resp_data += html_end
     response = get_header(200).encode() + bytes(resp_data, 'utf-8')
     connectionSocket.send(response)
-    print('=== Response ===')
-    print(response)
+    logger.info("Request successfully served: Resposne Sent with the requested file atatched")
+    # use the below print statement for printing the response ( for debugging )
+    # print('=== Response ===')
+    # print(response)
     connectionSocket.close()
+    logger.info("Connection Closed: Connection with client " + str(addr) + " closed")
     times += 1
-    print("\n------- " + str(times) + " ------ Connection Closed ----------------\n")
+    # print("\n------- " + str(times) + " ------ Connection Closed ----------------\n")
 serverSocket.close()
